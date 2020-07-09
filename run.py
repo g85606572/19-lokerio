c4a09616b1781d3c6fe74588253a16771d241853
g85606572 <g85606572@gmail.com>
2020-07-10 02:30:50 +0700

    bdl1

--- run.py
+++ run.py
@@ -5,21 +5,16 @@ import time
 from database import*
 
 #garifin 
+#kanye
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST','GET'])
 def home():
-<<<<<<< HEAD
     jobstreet = db.reference("jobstreet").order_by_child("id").get().values()
     loker = db.reference("loker").order_by_child("id").get().values()
-    return render_template('index.html',data=data)
-=======
-    Loker = db.reference("loker").order_by_child("id").get().values()
-    jobstreet = db.reference("jobstreet").order_by_child("id").get().values()
-    return render_template('index.html',Loker=Loker)
->>>>>>> 7455dc8662d7eff0ae06c2e024e6a516719eb751
+    return render_template('index.html',loker=loker)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug=True, port=9900)
